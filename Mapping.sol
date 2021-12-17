dad26808d8b00190a3c59c7d7e16fcef9d8211e0
Michael Asiedu <michaelasiedu148@gmail.com>
2021-12-17 22:23:40 +0000

    first commit

---
+++ Mapping.sol
@@ -0,0 +1,22 @@
+pragma solidity <= 0.7.0;
+
+contract Mapping {
+    mapping (address => uint) public balancedSentByAddress;
+
+// smart contract can now receive money
+    function smartContractReceive () public payable {
+        balancedSentByAddress[msg.sender] += msg.value; 
+    }
+
+// get balance of the contract
+    function balanceOfSmartContract() public view returns(uint) {
+        return address(this).balance;
+    }
+
+// withdraw all money to any address by using accounts that has made deposits only
+  function withDrawAllMoney (address payable _to) public {
+     uint balanceToSend = balancedSentByAddress[msg.sender];
+     balancedSentByAddress[msg.sender] = 0;
+     _to.transfer(balanceToSend); 
+  }
+}
